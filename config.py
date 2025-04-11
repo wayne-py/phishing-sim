3f82af9875e81417a1bfa2cd6f9dd52961d84f59
wayne-py <segunadedayo59@gmail.com>
2025-04-11 11:35:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 EMAIL_ADDRESS = 'youremail@example.com'
 EMAIL_PASSWORD = 'yourpassword'
-TRACKING_URL = 'http://localhost:5000/track?user='
+TRACKING_URL = 'clicked.html'
